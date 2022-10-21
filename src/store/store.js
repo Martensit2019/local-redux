a74a9ae875f414731115b7e1dd42a29ff36b7229
Martensit2019 <spichakandrey79@yandex.ru>
2022-10-21 17:22:34 +0300

    init

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import {createStore} from 'redux'
+import { taskReducer } from './taskReducer'
+
+const initialState = [
+  { id: 1, title: 'Task1', completed: false },
+  { id: 2, title: 'Task2', completed: false },
+]
+
+export function initialeStore(){
+  return createStore(taskReducer, initialState)
+}
---
+++ src/store/taskReducer.js
@@ -0,0 +1,18 @@
+import { taskUpdated, taskDeleted } from './actionTypes'
+
+export function taskReducer(state = [], action) {
+  switch (action.type) {
+    case taskUpdated: {
+      const newArr = [...state]
+      const elementIndex = newArr.findIndex((el) => el.id === action.payload.id)
+      newArr[elementIndex] = { ...newArr[elementIndex], ...action.payload }
+      return newArr
+    }
+    case taskDeleted: {
+      return [...state].filter((t) => t.id !== action.payload.id)
+    }
+
+    default:
+      return state
+  }
+}
